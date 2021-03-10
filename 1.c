e18e4597fa9fe2ea7cf422bd4816802a3a06c16c
4ernovArtem <chernov.artem.s@gmail.com>
2021-03-10 16:47:21 +0200

    first comit

---
+++ 1.c
@@ -0,0 +1,25 @@
+#include<stdio.h> // define the header file  
+
+int g (int x, int y) {
+  switch(x - y) {
+  case 0:
+    return x;
+  case 4:
+    y = y + 1;
+    break;
+  case 7:
+    x = x - 1;
+  case 9:
+    return x*y;
+  case 3:
+    y = x + 9;
+  default:
+    return y - x;
+  }
+return y;
+}
+
+void main()   // define the main function  
+{  
+    printf("%d",150);
+}  
